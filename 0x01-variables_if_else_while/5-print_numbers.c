0a53d424131c2d6a8548f2088e7c0be121de47a5
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-11 10:45:30 +0100

    Print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print digits in base 10
+ * author: Akpan Daniel
+ * Return: 0
+ */
+int main(void)
+{
+for (int num = 0; num < 10; num++)
+	printf("%i", num);
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print digits in base 10
+ * author: Akpan Daniel
+ * Return: 0
+ */
+int main(void)
+{
+for (int num = 48; num < 58; num++)
+	putchar(num);
+putchar('\n');
+return (0);
+}
