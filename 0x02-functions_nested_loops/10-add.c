3ded2ae61379cda95cf3ec0130ee506057619fdf
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-15 23:32:46 +0100

    Add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @n: first number
+ * @m: second number
+ * Return: n + m
+ * author: Akpan Daniel
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
