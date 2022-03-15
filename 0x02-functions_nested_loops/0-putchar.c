da671f8232f233363e1e33d0d46481d33d17b113
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-15 21:20:07 +0100

    Betty compliant

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * main - Output '_putchar' to console
+ * Return: 0
+ * author: Akpan Daniel
+ */
 int main(void)
 {
-	char[9] word = "_putchar";
+	char word[9] = "_putchar";
 	int n;
-	for(n = 0; n < 10; n++)
+
+	for (n = 0; n < 10; n++)
 	{
 		_putchar(word[n]);
 	}
