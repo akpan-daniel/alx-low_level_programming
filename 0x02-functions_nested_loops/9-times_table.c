8abf24f264c892b7c0d570eb3439ff4e0f24f6ea
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-15 23:22:06 +0100

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - Print 9 times table
+ * Return: void
+ * author: Akpan Daniel
+ */
+void times_table(void)
+{
+	int row;
+	int col;
+	int result = 0;
+
+	for (col = 0; col < 10; col++)
+	{
+		for (row = 0; row < 10; row++)
+		{
+			result = row * col;
+			if (result >= 10)
+			{
+				int first = result / 10;
+				int last = result % 10;
+				_putchar('0' + first);
+				_putchar('0' + last);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + (row * col));
+			}
+			if (row != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
