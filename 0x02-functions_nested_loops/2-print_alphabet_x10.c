e57749ecbe62a1c44830e8bb2f89324c4d09ff36
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-15 21:42:18 +0100

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Output a-z to console
+ * Return: 0
+ * author: Akpan Daniel
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 1; n <= 10; n++)
+	{
+		char word = 'a';
+
+		while (word <= 'z')
+		{
+			_putchar(word++);
+		}
+		_putchar('\n');
+	}
+}
