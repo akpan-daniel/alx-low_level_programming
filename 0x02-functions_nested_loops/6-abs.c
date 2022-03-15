c589dc1cb761b50fc14b50e6761db6150c655a27
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-15 22:18:03 +0100

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - Return the absolute value of a number
+ * @n: number to convert
+ * Return: absolute of n
+ * author: Akpan Daniel
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
