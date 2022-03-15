ff486cb72f3f2f74a9f5b55f910cc092ca5626f2
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-16 00:10:01 +0100

    Finish mandatory

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,27 +8,67 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n < 98)
 	{
-		if (n > 10)
+		while (n <= 98)
 		{
-			_putchar('0' + n / 10);
-			_putchar('0' + n % 10);
-		}
-		else
-		{
-			_putchar('0' + n);
-		}
+			if (n > 10)
+			{
+				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
+			}
+			else if (n < 0)
+			{
+				_putchar('-');
+				if (n <= -10)
+				{
+					_putchar('0' + ((n / 10) * -1));
+				}
+				_putchar('0' + ((n % 10) * -1));
+			}
+			else
+			{
+				_putchar('0' + n);
+			}
 
-		if (n < 98)
-		{
-			_putchar(',');
-			_putchar(' ');
+			if (n < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+			n++;
 		}
-		else
+	}
+	else
+	{
+		while (n >= 98)
 		{
-			_putchar('\n');
+			if (n > 100)
+			{
+				_putchar('0' + ((n / 10) / 10));
+				_putchar('0' + ((n / 10) % 10));
+				_putchar('0' + n % 10);
+			}
+			else
+			{
+				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
+			}
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+			n--;
 		}
-		n++;
 	}
 }
