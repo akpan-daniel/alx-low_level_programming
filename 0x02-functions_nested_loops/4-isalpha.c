47126884b03d84aca8ad55f0b6b6088d6546f54f
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-15 21:58:16 +0100

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabet
+ * @c: character to test
+ * Return: 1 if lower or upper case else 0
+ * author: Akpan Daniel
+ */
+int _islower(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
