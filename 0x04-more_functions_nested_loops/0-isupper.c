4f94c26b94c750993a1ead87e020773869015dc5
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-17 18:56:52 +0100

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase
+ * Return: 1 if upper else 0
+ * @c: Character to test
+ * author: Akpan Daniel
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90 ? 1 : 0);
+}
