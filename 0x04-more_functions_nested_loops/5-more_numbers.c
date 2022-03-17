6482ce5b910a9f1f4348da1c42452b091b36f4b5
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-17 19:45:21 +0100

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print 0 - 14 10x
+ * Return: void
+ * author: Akpan Daniel
+ */
+void more_numbers(void c)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		int num;
+
+		for (num = 0; num < 15; num++)
+		{
+			if (num < 10)
+			{
+				_putchar('0' + num);
+			}
+			else
+			{
+				_putchar('0' + num / 10);
+				_putchar('0' + num % 10);
+			}
+		}
+
+		_putchar('\n');
+	}
+}
