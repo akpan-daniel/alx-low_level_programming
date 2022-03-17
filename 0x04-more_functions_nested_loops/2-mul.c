7658aa73cfa20c7688de8479ace9392c703061c0
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-17 19:09:01 +0100

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two integers
+ * @a: First Integer
+ * @b: Second Integer
+ * Return: a multiplied by b
+ * author: Akpan Daniel
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
