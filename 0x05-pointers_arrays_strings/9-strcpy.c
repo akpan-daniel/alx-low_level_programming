59c5dfc1cbdfe30a4dcf7bed969e7e1abbabb73f
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-21 19:42:09 +0100

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy string
+ * @src: String to be copied
+ * @dest: String 
+ * Return: void
+ * author: Akpan Daniel
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
