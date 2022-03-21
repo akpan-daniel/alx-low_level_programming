42d96527da512dd5601b3291ebc8c33c6b86d2b0
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-21 17:56:43 +0100

    01

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Set value to 98
+ * @n: Pointer to int
+ * Return: void
+ * author: Akpan Daniel
+ */
+void reset_to_98(int *n)
+{
+	n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swap the values
+ * @a: First digit ti be swapped
+ * @b: Second digit to be swapped
+ * Return: void
+ * author: Akpan Daniel
+ */
+void swap_int(int *a, int *b)
+{
+	int *temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
