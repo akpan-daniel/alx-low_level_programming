bcd4ce192b0b8efc77843e27b9382801a139e5aa
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-21 18:57:06 +0100

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Output half a string
+ * @str: String to print
+ * Return: void
+ * author: Akpan Daniel
+ */
+void puts_half(char *str)
+{
+	char null = '\0';
+	int count, half;
+
+	for (count = 0; s[count] != '\0'; count++)
+		;
+
+	if ((count % 2) == 1)
+		half = (count - 1) / 2;
+	else
+		half = count / 2;
+
+	for (; half < count; half++)
+	{
+		_putchar(*(str + half));
+	}
+
+	_putchar('\n');
+}
