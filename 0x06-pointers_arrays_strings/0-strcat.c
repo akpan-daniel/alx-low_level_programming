482b3a09244aedf4e41728a08b4eeb7db8151d8a
Daniel Akpan <akpandaniel.n@gmail.com>
2022-03-23 21:19:36 +0100

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,20 +4,21 @@
  * _strcat - Append string dest by string src
  * @dest: String to be extended
  * @src: String to be appended
- * Return: char
+ * Return: char *
  * author: Akpan Daniel
  */
-char _strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i, j;
+int a = -1, i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
+for (i = 0; dest[i] != '\0'; i++)
+	;
 
-	for (j = 0; src[j] != '\0'; j++)
-		dest[i + j] = src[j];
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
 
-	dest[i + j] = src[i];
-
-	return (dest);
+return (dest);
 }
